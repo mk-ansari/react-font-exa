8daf4e95e81d1aba71aead899202dea20ed0b723
mk-ansari <109718096+mk-ansari@users.noreply.github.com>
2022-08-22 13:14:41 +0530

    react font example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import WebFont from "webfontloader";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto"],
+      },
+    });
+  }, []);
+  return <h1 className="font-loader">Hello</h1>;
 }
 
 export default App;
